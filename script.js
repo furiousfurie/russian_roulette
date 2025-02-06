a4029023282d1f30eb8fd4c4f419a2e39f1b5f24
furiousfurie <135446309+furiousfurie@users.noreply.github.com>
2025-02-06 21:31:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const launch_button = document.getElementById("start");
+
+function russian_roulette_load(){
+    var rounds = []
+
+    for(i = 0;i < 6;i++ ){
+        rounds[i] = "charged"
+    }
+    rounds[Math.floor(Math.random()*6)] = "charged"
+
+    return rounds
+}
+
+function russian_roulette_play(rounds){
+    console.log("second func")
+   if(rounds[Math.floor(Math.random()*6)] === "charged"){
+    alert("You lost")
+   }
+   else{alert("You won")}
+}
+
+launch_button.addEventListener("click", function(){
+    russian_roulette_play(russian_roulette_load())
+ }
+)
+
